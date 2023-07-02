359ee41ffa0db1e198253f23e5054068fa759a6e
codesanta <jathurshan@protonmail.com>
2023-07-02 08:07:28 +0530

    producturl added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -46,7 +46,7 @@ export const login = (email, password) => async (dispatch) => {
 
         try {
             dispatch(loginRequest())
-            const { data }  = await axios.post(`https://auwshi.onrender.com/api/v1/login`,{email,password});
+            const { data }  = await axios.post(`/api/v1/login`,{email,password});
             dispatch(loginSuccess(data))
         } catch (error) {
             dispatch(loginFail(error.response.data.message))
@@ -68,7 +68,7 @@ export const register = (userData) => async (dispatch) => {
             }
         }
 
-        const { data }  = await axios.post(`https://auwshi.onrender.com/api/v1/register`,userData, config);
+        const { data }  = await axios.post(`/api/v1/register`,userData, config);
         dispatch(registerSuccess(data))
     } catch (error) {
         dispatch(registerFail(error.response.data.message))
